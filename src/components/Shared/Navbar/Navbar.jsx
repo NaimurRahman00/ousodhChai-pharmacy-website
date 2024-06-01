6705bd30726195169d0e02cd0deee21bff891af3
Naimur00 <aminaim4u@gmail.com>
2024-06-01 11:08:24 +0600

    Navbar updated

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,19 +1,21 @@
 import Container from "../Container";
 import { AiOutlineMenu } from "react-icons/ai";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
 import avatarImg from "../../../assets/images/placeholder.jpg";
 import { IoLanguage } from "react-icons/io5";
 import Select from "../../Mini/Select";
+import { CiSearch } from "react-icons/ci";
+import { RiShoppingCartLine } from "react-icons/ri";
 
 const Navbar = () => {
   const { user, logOut } = useAuth();
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="fixed w-full bg-white z-10 shadow-sm">
-      <div className="py-4 border-b-[1px]">
+    <div className="fixed left-0 right-0 max-w-[2160px] mx-auto z-10">
+      <div className="py-5 bg-zinc-100 m-6 rounded-3xl">
         <Container>
           <div className="flex flex-row  items-center justify-between gap-3 md:gap-0">
             <div className="flex items-center">
@@ -32,18 +34,63 @@ const Navbar = () => {
                   Ousodh<span>Chai</span>
                 </h2>
               </div>
-              <div className="pl-6">
-                <h2 className="flex items-center text-lg gap-2 font-medium">
-                  <IoLanguage className="text-xl"/> Select Language
+              <div className="pl-6 flex flex-col items-end">
+                <h2 className="flex items-center text-base gap-2">
+                  <IoLanguage className="text-xl" /> Select Language
                 </h2>
                 <Select></Select>
               </div>
+              <div className="ml-6 w-80">
+                <label className="input input-bordered flex items-center gap-2 bg-white rounded-full w-full overflow-hidden border border-black/10">
+                  <div className="bg-lime-400 p-2.5 font-thin rounded-full m-1 cursor-pointer">
+                    <CiSearch className="text-xl" />
+                  </div>
+                  <input
+                    type="text"
+                    className="grow outline-none pr-4"
+                    placeholder="Medicine and healthcare items"
+                  />
+                </label>
+              </div>
             </div>
+
             {/* Dropdown Menu */}
-            <div className="relative">
+            <div className="relative flex gap-10 items-center">
+              <ul className="flex gap-10 items-center">
+                <li>
+                  <NavLink
+                    to="/"
+                    className={({ isActive }) =>
+                      isActive
+                        ? "text-xl font-medium text-lime-600 hover:text-lime-700"
+                        : "text-xl font-medium hover:text-black/70"
+                    }
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/d"
+                    className={({ isActive }) =>
+                      isActive
+                        ? "text-xl font-medium text-lime-600 hover:text-lime-700"
+                        : "text-xl font-medium hover:text-black/70"
+                    }
+                  >
+                    Shop
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="flex gap-2 items-center text-xl font-medium transition-all">
+                    <RiShoppingCartLine className="text-2xl" />
+                    Cart
+                  </NavLink>
+                </li>
+              </ul>
               <div className="flex flex-row items-center gap-3">
                 {/* Become A Host btn */}
-                <div className="hidden md:block">
+                {/* <div className="hidden md:block">
                   {!user && (
                     <button
                       disabled={!user}
@@ -52,25 +99,31 @@ const Navbar = () => {
                       Host your home
                     </button>
                   )}
-                </div>
+                </div> */}
                 {/* Dropdown btn */}
-                <div
-                  onClick={() => setIsOpen(!isOpen)}
-                  className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
-                >
-                  <AiOutlineMenu />
-                  <div className="hidden md:block">
-                    {/* Avatar */}
-                    <img
-                      className="rounded-full"
-                      referrerPolicy="no-referrer"
-                      src={user && user.photoURL ? user.photoURL : avatarImg}
-                      alt="profile"
-                      height="30"
-                      width="30"
-                    />
+                {user ? (
+                  <div
+                    onClick={() => setIsOpen(!isOpen)}
+                    className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+                  >
+                    <AiOutlineMenu />
+                    <div className="hidden md:block">
+                      {/* Avatar */}
+                      <img
+                        className="rounded-full"
+                        referrerPolicy="no-referrer"
+                        src={user && user.photoURL ? user.photoURL : avatarImg}
+                        alt="profile"
+                        height="30"
+                        width="30"
+                      />
+                    </div>
                   </div>
-                </div>
+                ) : (
+                  <Link className="text-xl font-medium bg-lime-400 px-4 py-2 rounded-2xl hover:scale-105 transition-all">
+                    Join Us
+                  </Link>
+                )}
               </div>
               {isOpen && (
                 <div className="absolute rounded-xl shadow-md w-[40vw] md:w-[10vw] bg-white overflow-hidden right-0 top-12 text-sm">
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Shared/Navbar/Navbar'
 import Footer from '../components/Shared/Footer/Footer'
 const Main = () => {
   return (
-    <div>
+    <div className='container max-w-[2160px] mx-auto'>
       <Navbar />
       <div className='pt-24 min-h-[calc(100vh-68px)]'>
         <Outlet />
