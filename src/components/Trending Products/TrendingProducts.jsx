d32e4170cc599a01dd9123c94fedd91e9ec63956
Naimur00 <aminaim4u@gmail.com>
2024-06-02 15:36:12 +0600

    Tending product section responsive

--- src/components/Trending Products/TrendingProducts.jsx
+++ src/components/Trending Products/TrendingProducts.jsx
@@ -8,13 +8,14 @@ const TrendingProducts = () => {
   return (
     <Container>
       <div className="mx-3 md:mx-20 py-10 md:py-24">
-        <div className="flex justify-between items-center mb-4 md:mb-16 ">
+        <div className="md:flex md:justify-between items-center mb-6 md:mb-16 ">
           <h1 className="text-[#252b61] text-3xl md:text-5xl font-extrabold text-center md:text-start">
             Trending products <br /> for you!
           </h1>
-          <Link>
+          <Link className="hidden md:flex">
             <h2 className="flex items-center gap-3 uppercase px-4 py-2 rounded-xl text-black/70 font-bold text-xl border border-white hover:border hover:border-[#252b61]">
-              See All Products<FaArrowRightLong />
+              See All Products
+              <FaArrowRightLong />
             </h2>
           </Link>
         </div>
@@ -27,6 +28,14 @@ const TrendingProducts = () => {
           {products.map((product, index) => (
             <Card key={index}></Card>
           ))}
+        </div>
+        <div className="mt-6 flex justify-center items-center">
+          <Link className="">
+            <h2 className="flex items-center gap-3 uppercase px-4 py-2 rounded-xl text-white/80 font-semibold text-xl bg-[#252b61] border border-[#252b61] hover:bg-white hover:text-black/85">
+              See All Products
+              <FaArrowRightLong />
+            </h2>
+          </Link>
         </div>
       </div>
     </Container>
