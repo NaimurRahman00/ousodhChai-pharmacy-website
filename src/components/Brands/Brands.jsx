2547b1fcb903ec76dfa36d9d901bb0beed9218f3
Naimur00 <aminaim4u@gmail.com>
2024-06-02 14:50:19 +0600

    Featured brand responsive

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -4,19 +4,19 @@ const Brands = () => {
   const brands = [1, 2, 3, 4, 5, 6, 7];
   return (
     <Container>
-      <div className="mx-20">
+      <div className="mx-3 md:mx-20">
         <div>
-          <h1 className="text-[#252b61] text-2xl md:text-6xl font-bold mb-4 md:mb-16 text-center md:text-start">
+          <h1 className="text-[#252b61] text-3xl md:text-6xl font-bold mb-4 md:mb-16 text-center md:text-start">
             Featured Brands
           </h1>
         </div>
-        <div className="flex gap-4 items-center justify-between">
+        <div className="flex gap-4 items-center justify-between overflow-x-auto w-full">
           {brands.map((brand, index) => (
             <div key={index}>
-              <div className="bg-[#f1f5f9] rounded-full size-40 flex justify-center items-center">
+              <div className="bg-[#f1f5f9] rounded-full size-20 md:size-40 flex justify-center items-center">
                 <img src="" alt="Brand Logo" />
               </div>
-              <h2 className="text-xl font-bold text-center text-black/70 mt-2">Brand Name</h2>
+              <h2 className="text-sm md:text-xl font-bold text-center text-black/70 mt-2">Brand Name</h2>
             </div>
           ))}
         </div>
