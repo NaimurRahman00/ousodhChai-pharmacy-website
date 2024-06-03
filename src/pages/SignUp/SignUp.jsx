8db71187a652d3838066c1b86741ad68ddc4a1a6
Naimur00 <aminaim4u@gmail.com>
2024-06-03 22:48:43 +0600

    Register page authentication

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,11 +1,53 @@
-import { Link } from "react-router-dom";
-import { FcGoogle } from "react-icons/fc";
+import { useContext } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+
+// import toast from "react-hot-toast
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state || "/";
+  const { createUser, user, setUser, updateUserProfile, loading } =
+    useContext(AuthContext);
+
+  // Email password sign in
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const name = form.name.value;
+    const photo = form.photo.value;
+    console.log({ name, email, photo, password });
+    try {
+      if (!/^.{6,}$/.test(password)) {
+        // return toast.error("Password length must minimum 6 letter!");
+        return
+      }
+      if (!/^(?=.*[A-Z]).+$/.test(password)) {
+        // return toast.error("Password must have an Uppercase letter!");
+        return
+      }
+      if (!/^(?=.*[a-z]).+$/.test(password)) {
+        // return toast.error("Password must have a Lowercase letter!");
+        return
+      }
+      await createUser(email, password);
+      await updateUserProfile(name, photo);
+      setUser({ ...user, photoURL: photo, displayName: name });
+      navigate(from, { replace: true });
+      // toast.success("Sign up successful!");
+    } catch (err) {
+      console.log(err);
+      // toast.error(err?.message);
+    }
+  };
+
   return (
     <div className="relative flex justify-center items-center min-h-screen bg-gradient-to-t from-[#16191c] to-[#173003] overflow-hidden">
       <div className="bg-black/50 w-full min-h-screen flex justify-center items-center backdrop-blur-[5rem] z-20">
-        <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-transparent text-gray-900 border-black/50 border w-[40rem] shadow-2xl shadow-black scale-[0.9]">
+        <div className="flex flex-col p-6 rounded-md sm:p-10 bg-transparent text-gray-900 border-black/50 border w-[32rem] shadow-2xl shadow-black scale-[0.9]">
           <div className="mb-8 text-center">
             <h1 className="my-3 text-4xl font-bold text-[#9fe870]">Sign up</h1>
             <p className="text-sm text-lime-100/70">
@@ -13,6 +55,7 @@ const SignUp = () => {
             </p>
           </div>
           <form
+          onSubmit={handleSignUp}
             noValidate=""
             action=""
             className="space-y-6 ng-untouched ng-pristine ng-valid"
@@ -54,10 +97,11 @@ const SignUp = () => {
               </div>
               {/* Photo upload */}
               <div>
-                <label htmlFor="image" className="block text-sm text-[#72bb45]">
+                <label htmlFor="photo" className="block text-sm text-[#72bb45]">
                   Image
                 </label>
                 <input
+                name="photo"
                   type="file"
                   className="block w-full px-3 py-2 mt-2 text-sm text-gray-600 bg-white border border-gray-300 rounded-lg file:bg-gray-200 file:text-gray-700 file:text-sm file:px-4 file:py-1 file:border-none file:rounded-full dark:file:bg-[#9fe870] dark:file:text-black file:font-semibold dark:text-gray-300 placeholder-gray-400/70 dark:placeholder-gray-500 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-300 dark:bg-transparent dark:focus:border-blue-300"
                 />
@@ -66,7 +110,6 @@ const SignUp = () => {
               <select className="select select-success w-full bg-transparent text-white/70 border-gray-300 focus:outline-none focus:ring-none">
                 <option className="text-black bg-[#9fe870]">User</option>
                 <option className="text-black bg-[#9fe870]">Seller</option>
-      
               </select>
               <div>
                 <div className="flex justify-between">
