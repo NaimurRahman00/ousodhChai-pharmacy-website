3b4e5f6a86424c03f5ffad17f5d8bba25d8e7cfd
Naimur00 <aminaim4u@gmail.com>
2024-06-03 14:32:07 +0600

    Footer added

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,11 +1,28 @@
+import Container from "../Container";
+import FooterComp from "./FooterComp";
+
 const Footer = () => {
   return (
-    <footer className='px-4 divide-y  text-gray-800 relative bottom-0 left-0'>
-      <div className='py-6 text-sm text-center text-gray-400'>
-        © 2024-2025 StayVista Inc. All rights reserved.
+    <Container>
+      <div className="bg-gradient-to-t from-[#153200] to-[#163300] p-[60px] rounded-[4rem] mt-28 mb-8">
+        <div>
+          <FooterComp></FooterComp>
+        </div>
+        <div className="flex justify-center items-center gap-8 bg-[#9fe870] rounded-[2.5rem] mt-4">
+          <div>
+            <img src="medicine.png" alt="" />
+          </div>
+          <h2 className="text-[8rem] text-[#163300] font-bold py-20">
+            OushodhChai
+          </h2>
+        </div>
+        <div className="flex justify-between items-center mt-10 text-[#9fe870]">
+          <p>&copy; 2024 OusodhChai</p>
+          <p>All Rights Reserved</p>
+        </div>
       </div>
-    </footer>
-  )
-}
+    </Container>
+  );
+};
 
-export default Footer
+export default Footer;
---
+++ src/components/Shared/Footer/FooterComp.jsx
@@ -0,0 +1,241 @@
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa6";
+
+const FooterComp = () => {
+  return (
+    <footer className="bg-white rounded-[2.5rem]">
+      <div className="container p-16 mx-auto">
+        <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <div>
+            <div className="text-lg font-bold text-[#1c1f1a] uppercase">
+              Company
+            </div>
+
+            <a
+              href="#"
+              className="block mt-5 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Installation
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Release Notes
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Upgrade Guide
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Using with Preprocessors
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Optimizing for Production
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Browser Support
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              IntelliSense
+            </a>
+          </div>
+
+          <div>
+            <div className="text-lg font-bold text-[#1c1f1a] uppercase">
+              Community
+            </div>
+
+            <a
+              href="#"
+              className="block mt-5 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Installation
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Release Notes
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Upgrade Guide
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Using with Preprocessors
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Optimizing for Production
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Browser Support
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              IntelliSense
+            </a>
+          </div>
+
+          <div>
+            <div className="text-lg font-bold text-[#1c1f1a] uppercase">
+              Support
+            </div>
+
+            <a
+              href="#"
+              className="block mt-5 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Installation
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Release Notes
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Upgrade Guide
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Using with Preprocessors
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Optimizing for Production
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Browser Support
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              IntelliSense
+            </a>
+          </div>
+
+          <div>
+            <div className="text-lg font-bold text-[#1c1f1a] uppercase">
+              Trust & Legal
+            </div>
+
+            <a
+              href="#"
+              className="block mt-5 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Installation
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Release Notes
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Upgrade Guide
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Using with Preprocessors
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Optimizing for Production
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              Browser Support
+            </a>
+            <a
+              href="#"
+              className="block mt-3 text-sm font-medium text-gray-500 duration-700  hover:text-[#1c1f1a] dark:hover:text-gray-200 hover:underline"
+            >
+              IntelliSense
+            </a>
+          </div>
+        </div>
+
+        <div className="sm:flex sm:items-center sm:justify-between mt-16">
+          <p className="text-sm text-[#1c1f1a]">
+            OusodhChai
+          </p>
+
+          <div className="flex mt-3 -mx-2 sm:mt-0">
+            <a
+              href="#"
+              className="mx-2 text-[#1c1f1a] hover:text-gray-500 bg-[#f1f5f9] p-4 rounded-full text-2xl"
+              aria-label="instagram"
+            >
+              <FaInstagram></FaInstagram>
+            </a>
+
+            <a
+              href="#"
+              className="mx-2 text-[#1c1f1a] hover:text-gray-500 bg-[#f1f5f9] p-4 rounded-full text-2xl"
+              aria-label="twitter"
+            >
+              <FaTwitter></FaTwitter>
+            </a>
+
+            <a
+              href="#"
+              className="mx-2 text-[#1c1f1a] hover:text-gray-500 bg-[#f1f5f9] p-4 rounded-full text-2xl"
+              aria-label="facebook"
+            >
+              <FaFacebook></FaFacebook>
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default FooterComp;
