4826c1ac9c247cd0187ac9be15b30c01b117518c
Naimur00 <aminaim4u@gmail.com>
2024-06-03 20:14:29 +0600

    Google authentication

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,51 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
+import useAuth from "../../hooks/useAuth";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const { user, signIn, signInWithGoogle, loading } = useContext(AuthContext);
+  const location = useLocation();
+  const from = location.state || "/";
+
+  // Go to homepage if you are already login
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+      // toast.success('Already logged in!')
+    }
+  }, [navigate, user]);
+
+  // Google sign in
+  const handleGoogleSignIn = async () => {
+    try {
+      await signInWithGoogle();
+      // toast.success('Log in successful!')
+      navigate(from, {replace: true});
+    } catch (err) {
+      console.log(err);
+      // toast.error(err?.message)
+    }
+  };
+
+    // Email password sign in
+    const handleSignIn = async e => {
+      e.preventDefault();
+      const form = e.target;
+      const email = form.email.value;
+      const password = form.password.value;
+      try {
+        const result = await signIn(email, password)
+        console.log(result)
+        navigate('/')
+        // toast.success('Sign in successful!')
+      } catch (err) {
+        console.log(err)
+        // toast.error(err?.message)
+      }
+    }
   return (
     <div className="relative flex justify-center items-center min-h-screen bg-gradient-to-t from-[#16191c] to-[#173003] overflow-hidden">
       <div className="bg-black/50 w-full min-h-screen flex justify-center items-center backdrop-blur-[5rem] z-20">
@@ -56,7 +100,7 @@ const Login = () => {
               </div>
             </div>
 
-            <div>
+            <div onClick={handleSignIn}>
               <button
                 type="submit"
                 className="bg-[#9fe870] font-bold text-xl w-full rounded-md py-3 text-[#16191c] hover:bg-transparent border border-black/0 hover:border-[#9fe870] transition-all hover:text-[#9fe870]"
@@ -77,7 +121,11 @@ const Login = () => {
             </p>
             <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
           </div>
-          <div className="flex justify-center items-center space-x-2 border my-4 p-2 border-gray-300 border-rounded cursor-pointer bg-black/5 hover:bg-[#9fe870] hover:border-transparent py-2 transition-all rounded-lg">
+          {/* Google login */}
+          <div
+            onClick={handleGoogleSignIn}
+            className="flex justify-center items-center space-x-2 border my-4 p-2 border-gray-300 border-rounded cursor-pointer bg-black/5 hover:bg-[#9fe870] hover:border-transparent py-2 transition-all rounded-lg"
+          >
             <FcGoogle size={32} />
 
             <p className="text-black/85 font-bold">Continue with Google</p>
