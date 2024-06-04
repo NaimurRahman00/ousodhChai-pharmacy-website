8f3bfcd43883b5c84136720fecbc78a46424f9dc
Naimur00 <aminaim4u@gmail.com>
2024-06-04 14:14:30 +0600

    Product details page added

--- src/pages/RoomDetails/RoomDetails.jsx
+++ src/pages/RoomDetails/RoomDetails.jsx
@@ -1,109 +1,60 @@
-import Container from '../../components/Shared/Container'
-import { Helmet } from 'react-helmet-async'
-import RoomReservation from '../../components/RoomDetails/RoomReservation'
-import Heading from '../../components/Shared/Heading'
+import { FaFire, FaRegHeart } from "react-icons/fa6";
+import Container from "../../components/Shared/Container";
+import { Helmet } from "react-helmet-async";
+import Rating from "../../components/Mini/Rating";
+import { RiHome2Line } from "react-icons/ri";
 
-// single room object (Fake Data)
-const room = {
-  location: 'Pattaya, Thailand',
-  category: 'Beach',
-  title: 'A15 thebase Sky Pool 1 Bedroom 1/1/Downtown Direct Beach',
-  to: '2024-05-21T18:00:00.000Z',
-  from: '2024-05-19T18:00:00.000Z',
-  price: '100',
-  guests: '2',
-  bathrooms: '1',
-  bedrooms: '1',
-  host: {
-    name: 'Shakil Ahmed Atik',
-    image:
-      'https://lh3.googleusercontent.com/a/ACg8ocJL_MZYn95fgATgHT_bWH8Em42gc8quAT57rhhHo4w9-lc-x8G-=s96-c',
-    email: 'shakilatik.ph@gmail.com',
-  },
-  description:
-    'Seamlessly evisculate frictionless e-markets through tactical interfaces. Holisticly visualize viral potentialities without mission-critical services.',
-  image: 'https://i.ibb.co/BsLQWH6/992ceffe-86d2-42b0-93b8-c24427806cca.webp',
-}
 const RoomDetails = () => {
   return (
     <Container>
       <Helmet>
-        <title>{room?.title}</title>
+        <title>OushodhChai | Details</title>
       </Helmet>
-      {room && (
-        <div className='max-w-screen-lg mx-auto'>
-          {/* Header */}
-          <div className='flex flex-col gap-6'>
-            <div>
-              <Heading title={room.title} subtitle={room.location} />
-              <div className='w-full md:h-[60vh] overflow-hidden rounded-xl'>
-                <img
-                  className='object-cover w-full'
-                  src={room.image}
-                  alt='header image'
-                />
-              </div>
-            </div>
-          </div>
-          <div className='grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6'>
-            {/* Room Info */}
-            <div className='col-span-4 flex flex-col gap-8'>
-              <div className='flex flex-col gap-2'>
-                <div
-                  className='
-                text-xl 
-                font-semibold 
-                flex 
-                flex-row 
-                items-center
-                gap-2
-              '
-                >
-                  <div>Hosted by {room?.host?.name}</div>
-
-                  <img
-                    className='rounded-full'
-                    height='30'
-                    width='30'
-                    alt='Avatar'
-                    src={room?.host?.image}
-                  />
-                </div>
-                <div
-                  className='
-                flex 
-                flex-row 
-                items-center 
-                gap-4 
-                font-light
-                text-neutral-500
-              '
-                >
-                  <div>{room?.guests} guests</div>
-                  <div>{room?.bedrooms} rooms</div>
-                  <div>{room?.bathrooms} bathrooms</div>
-                </div>
-              </div>
-
-              <hr />
-              <div
-                className='
-          text-lg font-light text-neutral-500'
-              >
-                {room?.description}
-              </div>
-              <hr />
-            </div>
-
-            <div className='md:col-span-3 order-first md:order-last mb-10'>
-              {/* RoomReservation */}
-              <RoomReservation room={room} />
-            </div>
+      <div className="flex gap-4 bg-[#f4f4f5] rounded-t-[3rem]">
+        <div className="relative flex-1 flex items-center justify-center rounded-xl py-10">
+          <img
+            src="https://i.ibb.co/WfJbFN3/Screenshot-2024-06-02-202353-removebg-preview.png"
+            alt=""
+            className="w-[30rem]"
+          />
+          <div className="absolute top-10 left-10 text-2xl hover:text-red-500 cursor-pointer">
+            <FaRegHeart />
           </div>
         </div>
-      )}
+        <div className="flex-1 rounded-xl py-10">
+          <p className="text-lg font-bold text-black/70">Product Genre</p>
+          <h2 className="text-2xl font-bold text-black/80">Product Name</h2>
+          <h2 className="text-base font-medium text-black/50">Company Name</h2>
+          <h2 className="mt-10">
+            <span className="text-4xl font-bold text-black/85">$99.50</span> <del className="text-black/40 font-semibold text-lg ms-2">$120.99</del>
+          </h2>
+          <div className="flex gap-6 items-center mt-4">
+            <Rating></Rating>
+            <p>5.00 | 7 Reviews</p>
+            <p className="underline text-blue-400 hover:text-blue-600 ms-3">Write a review</p>
+          </div>
+          <h4 className="underline text-blue-400 hover:text-blue-600 mt-3">Check Stores availability</h4>
+          <div className="flex items-center gap-4 py-4 px-6 my-4 bg-[#9fe870] md:w-[29rem] rounded-2xl font-bold text-black/90 text-xl">
+            <RiHome2Line className="text-2xl text-black/90"/>
+            <h2>Home delivery within 2 Hours</h2>
+          </div>
+          <div className="flex items-center gap-4">
+            <div className="join">
+              <button className="btn join-item text-3xl text-[#77c049] bg-[#9fe870] hover:bg-[#79c44a]">-</button>
+              <h2 className="join-item text-2xl hover:bg-transparent px-6 py-2">1</h2>
+              <button className="btn join-item text-3xl text-[#77c049] bg-[#9fe870] hover:bg-[#79c44a]">+</button>
+            </div>
+            <button className="btn bg-[#9fe870] hover:bg-[#79c44a] text-xl font-semibold ml-4">Add to Cart</button>
+            <button className="btn bg-[#9fe870] hover:bg-[#79c44a] text-xl font-semibold">Buy Now</button>
+          </div>
+          <div className="flex items-center gap-4 border border-[#5f9a3b] rounded-xl px-6 py-4 my-4 text-xl font-medium w-fit">
+            <FaFire className="text-orange-600 text-2xl"/>
+            <h3>Running fast! 100+ sold in the last 7 days</h3>
+          </div>
+        </div>
+      </div>
     </Container>
-  )
-}
+  );
+};
 
-export default RoomDetails
+export default RoomDetails;
