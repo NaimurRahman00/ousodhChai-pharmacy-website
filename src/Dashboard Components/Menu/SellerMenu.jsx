d27a85ca2920b5ac4a799337009f7747d6d00d06
Naimur00 <aminaim4u@gmail.com>
2024-06-06 12:21:09 +0600

    Seller menu items added in sellerMenu page

--- src/Dashboard Components/Menu/SellerMenu.jsx
+++ src/Dashboard Components/Menu/SellerMenu.jsx
@@ -1,9 +1,42 @@
+import { GoHomeFill } from "react-icons/go";
+import { NavLink } from "react-router-dom";
+import { AiFillMedicineBox } from "react-icons/ai";
+import { FaHistory } from "react-icons/fa";
+import { RiAdvertisementFill } from "react-icons/ri";
 
 const SellerMenu = () => {
     return (
-        <div>
-            this is seller menu
-        </div>
+        <nav>
+        <NavLink
+          to="dashboardHome"
+          className="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+          href="#"
+        >
+          <GoHomeFill />
+          <span className="mx-4 font-medium">Home</span>
+        </NavLink>
+        <NavLink
+          className="flex items-center px-4 py-2 mt-5 text-gray-800 transition-colors duration-300 transform rounded-lg dark:text-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+          href="#"
+        >
+          <AiFillMedicineBox />
+          <span className="mx-4 font-medium">Manage Medicines</span>
+        </NavLink>
+        <NavLink
+          className="flex items-center px-4 py-2 mt-5 text-gray-800 transition-colors duration-300 transform rounded-lg dark:text-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+          href="#"
+        >
+          <FaHistory />
+          <span className="mx-4 font-medium">Payment History</span>
+        </NavLink>
+        <NavLink
+          className="flex items-center px-4 py-2 mt-5 text-gray-800 transition-colors duration-300 transform rounded-lg dark:text-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+          href="#"
+        >
+          <RiAdvertisementFill />
+          <span className="mx-4 font-medium">Advertisement</span>
+        </NavLink>
+      </nav>
     );
 };
 
