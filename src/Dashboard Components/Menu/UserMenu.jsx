4d0f07d5e234e4e3f78e113657f510311ad28458
Naimur00 <aminaim4u@gmail.com>
2024-06-06 12:26:50 +0600

    User menu items added in UserMenu page

--- src/Dashboard Components/Menu/UserMenu.jsx
+++ src/Dashboard Components/Menu/UserMenu.jsx
@@ -1,10 +1,20 @@
+import { FaHistory } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const UserMenu = () => {
-    return (
-        <div>
-            this is User menu
-        </div>
-    );
+  return (
+    <nav>
+      <Link
+        to="dashboardHome"
+        className="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+        href="#"
+      >
+        <FaHistory />
+        <span className="mx-4 font-medium">Payment History</span>
+      </Link>
+      
+    </nav>
+  );
 };
 
 export default UserMenu;
