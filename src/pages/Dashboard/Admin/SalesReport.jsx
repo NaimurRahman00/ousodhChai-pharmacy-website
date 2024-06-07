6d24d27a45d96dd9a1902325a0ee737ec346bbd7
Naimur00 <aminaim4u@gmail.com>
2024-06-07 08:04:16 -0600

    Admin dashboard sales report page updated

--- src/pages/Dashboard/Admin/SalesReport.jsx
+++ src/pages/Dashboard/Admin/SalesReport.jsx
@@ -1,4 +1,43 @@
+import DataTable from "react-data-table-component";
+// import data from '../../constants/sampleMovieData';
+
 const SalesReport = () => {
+  const columns = [
+    {
+      name: "Medicine Name",
+      selector: (row) => row.title,
+      sortable: true,
+    },
+    {
+      name: "Seller Email",
+      selector: (row) => row.email,
+      sortable: true,
+    },
+    {
+      name: "Buyer Email",
+      selector: (row) => row.year,
+      sortable: true,
+    },
+    {
+      name: "Total Price",
+      selector: (row) => row.year,
+      sortable: true,
+    },
+  ];
+  const data = [
+    {
+      title: "Napa",
+      email : "seller@gmail.com",
+    },
+    {
+      title: "Tahlil",
+      selector: "20",
+    },
+    {
+      name: "Mahfuz",
+      age: "20",
+    },
+  ];
   return (
     <div className="p-8">
       <div className="flex items-center justify-between mb-8">
@@ -21,59 +60,13 @@ const SalesReport = () => {
           </select>
         </div>
       </div>
-      <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 mt-4">
-        <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-          <div className="overflow-hidden border-2 border-[#9fe870] md:rounded-2xl">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-[#f1f5f9]">
-                <tr>
-                  <th
-                    scope="col"
-                    className="py-3.5 px-4 text-base font-semibold text-left rtl:text-right text-black"
-                  >
-                    <button className="flex items-center gap-x-3 focus:outline-none">
-                      <span>Medicine name</span>
-                    </button>
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-12 py-3.5 text-base font-semibold text-left text-black"
-                  >
-                    Seller email
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-4 py-3.5 text-base font-semibold text-left  text-black"
-                  >
-                    Buyer email
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-4 py-3.5  text-base font-semibold text-left rtl:text-right text-black"
-                  >
-                    Total price
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
-                <tr>
-                  <td className="px-4 py-4 text-base font-medium whitespace-nowrap">
-                    Monocast 10
-                  </td>
-                  <td className="px-12 py-4 text-base font-medium whitespace-nowrap">
-                    seller@gmail.com
-                  </td>
-                  <td className="px-4 py-4 text-base font-medium whitespace-nowrap">
-                    buyer@email.com
-                  </td>
-                  <td className="px-4 py-4 text-base font-medium whitespace-nowrap">
-                    $299.00
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+      <div className="py-10 overflow-hidden border-2 border-[#9fe870] md:rounded-2xl font-bold">
+        <DataTable
+          columns={columns}
+          data={data}
+          pagination
+        />
+        ;
       </div>
     </div>
   );
