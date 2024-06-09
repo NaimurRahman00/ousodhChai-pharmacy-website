b98b995424bf5806376735c8af7d2f9a28b913ff
Naimur00 <aminaim4u@gmail.com>
2024-06-09 06:25:59 -0600

    Brands logo updated

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -1,7 +1,14 @@
 import Container from "../Shared/Container";
 
 const Brands = () => {
-  const brands = [1, 2, 3, 4, 5, 6, 7];
+  const brands = [{ logo: "https://i.ibb.co/1s6MGH8/download-removebg-preview-2.png"},
+    { logo: "https://i.ibb.co/cQtDXHB/download-removebg-preview-1.png"},
+    { logo: "https://i.ibb.co/9Z5GvC6/download-removebg-preview.png"},
+    { logo: "https://i.ibb.co/4ZCgV7F/beximco-pharmaceuticals-ltd-600-removebg-preview.png"},
+    { logo: "https://i.ibb.co/f13Gb0H/images-removebg-preview.png"},
+    { logo: "https://i.ibb.co/s5WHNHx/eskayef-removebg-preview.png"},
+    { logo: "https://i.ibb.co/pPsjxmZ/renata-limited-logo-removebg-preview.png"}
+    ]
   return (
     <Container>
       <div className="mx-3 md:mx-20">
@@ -13,8 +20,8 @@ const Brands = () => {
         <div className="flex gap-4 items-center justify-between overflow-x-auto w-full">
           {brands.map((brand, index) => (
             <div key={index}>
-              <div className="bg-[#f1f5f9] rounded-full size-20 md:size-40 flex justify-center items-center">
-                <img src="" alt="Brand Logo" />
+              <div className="bg-[#f1f5f9] rounded-full size-20 md:size-40 flex justify-center items-center overflow-hidden">
+                <img src={brand?.logo} alt="Brand Logo" width={100}/>
               </div>
               <h2 className="text-sm md:text-xl font-bold text-center text-black/70 mt-2">Brand Name</h2>
             </div>
