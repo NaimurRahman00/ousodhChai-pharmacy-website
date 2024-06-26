8a2b14a66e1523fb857c1381451b8be18c8b5afe
Naimur00 <aminaim4u@gmail.com>
2024-06-26 05:34:38 -0600

    add some animation in categories section

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -2,7 +2,12 @@ import Container from "../Shared/Container";
 import CategoriesCard from "./CategoriesCard.jsx";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
+
 const Categories = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
 
   // Getting data using TanStack queries
   const { data: categories = [], isLoading } = useQuery({
@@ -19,11 +24,31 @@ const Categories = () => {
   return (
     <Container>
       <div className="bg-white rounded-t-[2rem] z-30 mt-20 relative">
-        <div className="py-6 md:py-20 mx-4 md:px-16 relative flex gap-5 md:gap-6 items-center justify-between overflow-x-auto bg-white rounded-t-[2rem]">
+        <motion.div
+          ref={ref}
+          initial={{ opacity: 0 }}
+          animate={
+            isInView
+              ? {
+                  opacity: 1,
+                  transition: { staggerChildren: 0.2, delayChildren: 0.3 },
+                }
+              : { opacity: 0 }
+          }
+          transition={{ duration: 0.5, ease: "easeOut" }}
+          className="py-6 md:py-20 mx-4 md:px-16 relative flex gap-5 md:gap-6 items-center justify-between overflow-x-auto bg-white rounded-t-[2rem]"
+        >
           {categories.map((item, index) => (
-            <CategoriesCard key={index} item={item} index={index}></CategoriesCard>
+            <motion.div
+              initial={{ opacity: 0, y: 50 }} // Line 26
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, ease: "easeOut" }}
+              key={index}
+            >
+              <CategoriesCard item={item} index={index}></CategoriesCard>
+            </motion.div>
           ))}
-        </div>
+        </motion.div>
       </div>
     </Container>
   );
